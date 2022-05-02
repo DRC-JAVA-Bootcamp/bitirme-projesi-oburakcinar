c0ab062ec20e01722e24a54fbfffde8e2dfde734
Burak Cinar <burrakcinar@gmail.com>
2022-05-02 15:43:29 +0300

    adding member and role repository

---
+++ src/main/java/com/burak/recipe/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.burak.recipe.repository;
+
+import com.burak.recipe.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String roleName);
+}
