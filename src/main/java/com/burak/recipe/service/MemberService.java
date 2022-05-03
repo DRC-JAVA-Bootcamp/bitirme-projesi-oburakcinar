4851d74419e7b5d851c7ac5ac0fcaff04dc218eb
Burak Cinar <burrakcinar@gmail.com>
2022-05-03 10:59:54 +0300

    adding member update method to member service

--- src/main/java/com/burak/recipe/service/MemberService.java
+++ src/main/java/com/burak/recipe/service/MemberService.java
@@ -1,15 +1,22 @@
 package com.burak.recipe.service;
 
 import com.burak.recipe.entity.Member;
+import com.burak.recipe.requestDto.AddressSaveRequestDto;
 import com.burak.recipe.requestDto.MemberSaveRequestDto;
 import com.burak.recipe.responseDto.MemberDeleteResponseDto;
 import com.burak.recipe.responseDto.MemberSaveResponseDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface MemberService extends UserDetailsService {
-    public MemberSaveResponseDto save(MemberSaveRequestDto memberSaveRequestDto);
+    MemberSaveResponseDto save(MemberSaveRequestDto memberSaveRequestDto);
 
     Member findById(long memberId);
 
     MemberDeleteResponseDto deleteById(long memberId);
+
+    MemberSaveResponseDto updateMemberAddress(AddressSaveRequestDto addressSaveRequestDto);
+
+    List<Member> findAll();
 }
--- src/main/java/com/burak/recipe/service/MemberServiceImpl.java
+++ src/main/java/com/burak/recipe/service/MemberServiceImpl.java
@@ -1,27 +1,34 @@
 package com.burak.recipe.service;
 
+import com.burak.recipe.entity.Address;
 import com.burak.recipe.entity.Member;
 import com.burak.recipe.entity.Role;
 import com.burak.recipe.exception.BadRequestException;
 import com.burak.recipe.exception.EntityAlreadyExistsException;
 import com.burak.recipe.exception.EntityNotFoundException;
+import com.burak.recipe.repository.AddressRepository;
 import com.burak.recipe.repository.MemberRepository;
 import com.burak.recipe.repository.RoleRepository;
+import com.burak.recipe.requestDto.AddressSaveRequestDto;
 import com.burak.recipe.requestDto.MemberSaveRequestDto;
 import com.burak.recipe.responseDto.MemberDeleteResponseDto;
 import com.burak.recipe.responseDto.MemberSaveResponseDto;
+import com.burak.recipe.util.AuthenticationInfoUtil;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -33,9 +40,18 @@ public class MemberServiceImpl implements MemberService{
 
     private final RoleRepository roleRepository;
 
+    private final AddressRepository addressRepository;
+
     private final ModelMapper modelMapper;
 
     private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationInfoUtil authenticationInfoUtil;
+
+    @Override
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
 
     @Override
     public Member findById(long memberId) {
@@ -64,7 +80,7 @@ public class MemberServiceImpl implements MemberService{
         Member member = modelMapper.map(memberSaveRequestDto, Member.class);
 
         //setting member role
-        member.setRole(roleRepository.findByName("MEMBER"));
+        member.setRole(roleRepository.findByName("ROLE_MEMBER"));
 
         //encrypting the password
         member.setPassword(passwordEncoder.encode(member.getPassword()));
@@ -87,6 +103,33 @@ public class MemberServiceImpl implements MemberService{
         return memberDeleteResponseDto;
     }
 
+    @Override
+    public MemberSaveResponseDto updateMemberAddress(AddressSaveRequestDto addressSaveRequestDto) {
+
+        //getting username from basic authentication
+        String memberUserName = authenticationInfoUtil.getAuthenticationUserName();
+
+        //finding the number by user name
+        Member member = memberRepository.findByUserName(memberUserName);
+
+        //updating address
+        Address address = member.getAddress();
+        address.setCity(addressSaveRequestDto.getCity());
+        address.setCountry(addressSaveRequestDto.getCountry());
+        address.setStreet(addressSaveRequestDto.getStreet());
+        address.setZipCode(addressSaveRequestDto.getZipCode());
+
+        //saving address
+        addressRepository.save(address);
+
+        //creating response
+        MemberSaveResponseDto responseDto = modelMapper.map(member, MemberSaveResponseDto.class);
+
+        return responseDto;
+    }
+
+
+
 
     @Override
     @Transactional
@@ -104,4 +147,6 @@ public class MemberServiceImpl implements MemberService{
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
+
+
 }
