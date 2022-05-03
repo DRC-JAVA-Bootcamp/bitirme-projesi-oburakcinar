31f445bba27d5b45dbc496ee547333a53de3dd5f
Burak Cinar <burrakcinar@gmail.com>
2022-05-03 20:35:23 +0300

    changing class names

--- src/main/java/com/burak/recipe/validation/FoodCategory.java
+++ src/main/java/com/burak/recipe/validation/FoodCategory.java
@@ -7,14 +7,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = FoodTypeValidator.class)
+@Constraint(validatedBy = FoodCategoryValidator.class)
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface FoodType {
+public @interface FoodCategory {
     public String value() default "";
 
     //define default error message
-    public String message() default "Invalid food type";
+    public String message() default "Invalid food category";
 
     //define default groups, ---> groups can group related constraints
     public Class<?>[] groups() default {};
--- src/main/java/com/burak/recipe/validation/FoodCategoryValidator.java
+++ src/main/java/com/burak/recipe/validation/FoodCategoryValidator.java
@@ -1,18 +1,18 @@
 package com.burak.recipe.validation;
 
-import com.burak.recipe.entity.FoodTypeEnum;
+import com.burak.recipe.entity.FoodCategoryEnum;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class FoodTypeValidator implements ConstraintValidator<FoodType, String> {
+public class FoodCategoryValidator implements ConstraintValidator<FoodCategory, String> {
 
     //String foodType is the object to be validated
     @Override
-    public boolean isValid(String foodType, ConstraintValidatorContext context) {
-        foodType = foodType.toUpperCase();
+    public boolean isValid(String foodCategory, ConstraintValidatorContext context) {
+        foodCategory = foodCategory.toUpperCase();
         try {
-            if (FoodTypeEnum.valueOf(foodType) instanceof FoodTypeEnum) {
+            if (FoodCategoryEnum.valueOf(foodCategory) instanceof FoodCategoryEnum) {
                 return true; //means object is validated
             }
             return false;  //means object is not validated
