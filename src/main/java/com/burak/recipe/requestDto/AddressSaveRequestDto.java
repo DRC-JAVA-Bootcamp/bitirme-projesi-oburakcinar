25e99275aa01bc5cfe9892d41b704465cb02dea3
Burak Cinar <burrakcinar@gmail.com>
2022-05-03 10:58:37 +0300

    adding address save dto

---
+++ src/main/java/com/burak/recipe/requestDto/AddressSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.burak.recipe.requestDto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
+
+@Data
+public class AddressSaveRequestDto {
+
+
+    @NotEmpty(message = "country field cannot be empty")
+    private String country;
+
+    @NotEmpty(message = "city field cannot be empty")
+    private String city;
+
+    @NotEmpty(message = "street field cannot be empty")
+    private String street;
+
+    @Positive(message = "zipcode must be positive")
+    private Long zipCode;
+}
--- src/main/java/com/burak/recipe/requestDto/MemberSaveRequestDto.java
+++ src/main/java/com/burak/recipe/requestDto/MemberSaveRequestDto.java
@@ -3,20 +3,32 @@ package com.burak.recipe.requestDto;
 import com.burak.recipe.entity.Address;
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 public class MemberSaveRequestDto {
 
+    @NotEmpty(message = "name field cannot be empty")
     private String name;
 
+    @NotEmpty(message = "lastname field cannot be empty")
     private String lastName;
 
+    @NotEmpty(message = "userName field cannot be empty")
     private String userName;
 
+    @NotEmpty(message = "password field cannot be empty")
     private String password;
 
+    @NotNull
+    @Past(message = "birth date must be a past date")
     private LocalDate birthDate;
 
+    @Valid
+    @NotNull(message = "address field cannot be empty")
     private Address address;
 }
